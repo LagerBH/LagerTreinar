ea0dba7fff7987339b2887883ff6163c47a59818
ADM <ADM@MICRO134>
2017-01-23 19:04:39 -0200

    ssdsd

--- Project170113Treinar/Treinar_Arturo/src/CollectionActivity/HashMapTest.java
+++ Project170113Treinar/Treinar_Arturo/src/CollectionActivity/HashMapTest.java
@@ -1,23 +1,26 @@
 package CollectionActivity;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
 
 public class HashMapTest {
 	public static void main(String[] args) {
-		Map<String, String> m1 = new HashMap<>();
-		m1.put("Zara", "8");
-		m1.put("Mahnaz", "31");
-		m1.put("Ayan", "12");
-		m1.put("Daisy", "14");
-		m1.put("Daisy", "18");
-		System.out.println();
-		System.out.println(" Map Elements");
-		System.out.println("\t" + m1);
-		System.out.println(m1.keySet());
-		System.out.println(m1.values());
-		System.out.println(m1.size());
-		
-		
+		Character primLetra = null;
+		List<String> n1 = new LinkedList<>();
+		add(n1, "Jorge", "Henrique", "Jadine", "Gleidson", "Hugo", "Cristiano", "Arturo", "Artur", "Karina", "Bruno",
+				"Gabriel", "Cristiano");
+
+		for (int i = 0; i < n1.size(); i++) {
+			System.out.println("Primeria Letra".substring(0, 1));
+
+		}
+
+
+	}
+
+	private static void add(List<String> n1, String... string) {
+		for (String nome : string) {
+			n1.add(nome);
+		}
 	}
 }
